de23ff54ba31cabe044733b0958519771ece287f
IDrinkYourTea <legoemil123@gmail.com>
2023-05-08 12:56:51 +0200

    Add project files.

---
+++ JegGirOppExamenForTestEmil/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="JSONOppgava.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Emils vær</h1>
+            Temperaturen nå i den ene byen, Fredrikstad: <asp:Label ID="LabelTempNow" runat="server" Text="TempNowValue"></asp:Label> celsius
+            <br />
+            <br />
+            Vindstyrke nå i den ene byen, Fredrikstad: <asp:Label ID="LabelStyrkeNow" runat="server" Text="StyrkeNowValue"></asp:Label> m/s
+            <br />
+            <br />
+            Vindretningen nå i den ene byen, Fredrikstad: <asp:Label ID="LabelRetningNow" runat="server" Text="RetningNowValue"></asp:Label> degrees
+            <br />
+            <br />
+            Temp °C, Styrke m/s og Retning ° er i den ene byen, Fredrikstad: <asp:Label ID="LabelThreeNow" runat="server" Text="ThreeNowValue"></asp:Label> 
+        </div>
+    </form>
+</body>
+</html>
---
+++ JegGirOppExamenForTestEmil/Default.aspx.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JSONOppgava
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Denne labelen er lagt på i html-en "foran".
+            LabelTempNow.Text = GetNowTemp().ToString();//vi må ha tostring fordi labels text er string, mens temperatur er et tall, en int.
+            LabelStyrkeNow.Text = GetNowStyrke().ToString();
+            LabelRetningNow.Text = GetNowRetning().ToString();
+            LabelThreeNow.Text = GetNowThree().ToString();
+        }
+
+        //Oppgave 1 - henter ut temp nå
+        public int GetNowTemp()//denne har int, så vil da kunne returnere et heltall. Bør returnere en double, et desimaltall
+        {
+            //http://jsonviewer.stack.hu/
+            //https://peterdaugaardrasmussen.com/2022/01/18/how-to-get-a-property-from-json-using-selecttoken/
+            //create the httpwebrequest
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.met.no/weatherapi/nowcast/2.0/complete?lat=59.9333&lon=10.7166");
+            int temp = 0; //heltall. her bør det være en annen datatype. Fikser du det?
+
+            //the usual stuff. run the request, parse your json. with error handling
+            try
+            {
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "GET";
+                httpWebRequest.UserAgent = "bolle";
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    JObject jObj = JObject.Parse(result);
+                    JToken data = jObj.SelectToken("properties.timeseries[0].data.instant.details");
+
+                    temp = data.Value<int>("air_temperature");//key name står i " " - getting key.value
+                }
+                return temp;//returner ønsket verdi
+            }
+            catch { Exception ex; }
+            return 0;//returner ønsket verdi
+        }
+
+        public int GetNowStyrke()//denne har int, så vil da kunne returnere et heltall. Bør returnere en double, et desimaltall
+        {
+            //http://jsonviewer.stack.hu/
+            //https://peterdaugaardrasmussen.com/2022/01/18/how-to-get-a-property-from-json-using-selecttoken/
+            //create the httpwebrequest
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.met.no/weatherapi/nowcast/2.0/complete?lat=59.9333&lon=10.7166");
+            int styrke = 0; //heltall. her bør det være en annen datatype. Fikser du det?
+
+            //the usual stuff. run the request, parse your json. with error handling
+            try
+            {
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "GET";
+                httpWebRequest.UserAgent = "bolle";
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    JObject jObj = JObject.Parse(result);
+                    JToken data = jObj.SelectToken("properties.timeseries[0].data.instant.details");
+
+                    styrke = data.Value<int>("wind_speed");//key name står i " " - getting key.value
+                }
+                return styrke;//returner ønsket verdi
+            }
+            catch { Exception ex; }
+            return 0;//returner ønsket verdi
+        }
+
+        public double GetNowRetning()//denne har int, så vil da kunne returnere et heltall. Bør returnere en double, et desimaltall
+        {
+            //http://jsonviewer.stack.hu/
+            //https://peterdaugaardrasmussen.com/2022/01/18/how-to-get-a-property-from-json-using-selecttoken/
+            //create the httpwebrequest
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.met.no/weatherapi/nowcast/2.0/complete?lat=59.9333&lon=10.7166");
+            double retning = 0; //heltall. her bør det være en annen datatype. Fikser du det?
+
+            //the usual stuff. run the request, parse your json. with error handling
+            try
+            {
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "GET";
+                httpWebRequest.UserAgent = "bolle";
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    JObject jObj = JObject.Parse(result);
+                    JToken data = jObj.SelectToken("properties.timeseries[0].data.instant.details");
+
+                    retning = data.Value<double>("wind_from_direction");//key name står i " " - getting key.value
+                }
+                return retning;//returner ønsket verdi
+            }
+            catch { Exception ex; }
+            return 0;//returner ønsket verdi
+        }
+
+        public (int, int, double) GetNowThree()
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.met.no/weatherapi/nowcast/2.0/complete?lat=59.9333&lon=10.7166");
+            int temp = GetNowTemp();
+            int styrke = GetNowStyrke();
+            double retning = GetNowRetning();
+
+            return (temp, styrke, retning);
+        }
+    }
+}
